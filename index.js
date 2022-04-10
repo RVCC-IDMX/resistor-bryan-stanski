246e1259158b55a5299e15c2f6ae8c0fa59e4f6a
Maglen-gg <bryan.with.a.wi@gmail.com>
2022-04-10 12:56:57 -0400

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { getResistorOhms } from './resistor.js';
 
+// set default bands
 const defaultBands = {
   color1: 'red',
   color2: 'green',
@@ -7,11 +8,13 @@ const defaultBands = {
   tolerance: 'gold',
 };
 
+// resistor bands
 const resistorBands1 = document.querySelector('#b0');
 const resistorBands2 = document.querySelector('#b1');
 const resistorBands3 = document.querySelector('#b2');
 const resistorBands4 = document.querySelector('#b3');
 
+// color selects
 const band1 = document.querySelector('#color0');
 const band2 = document.querySelector('#color1');
 const multiplier = document.querySelector('#color2');
@@ -21,6 +24,7 @@ const answerElement = document.querySelector('.answer');
 
 let color;
 
+// band color 1
 band1.addEventListener('click', (e) => {
   color = e.target.className.replace('box ', '');
   resistorBands1.className = `band ${color}`;
@@ -28,22 +32,23 @@ band1.addEventListener('click', (e) => {
   answerElement.innerText = getResistorOhms(defaultBands);
 });
 
-//color 1
-
+// band color 2
 band2.addEventListener('click', (e) => {
   color = e.target.className.replace('box ', '');
   resistorBands2.className = `band ${color}`;
   defaultBands.color2 = color;
   answerElement.innerText = getResistorOhms(defaultBands);
 });
-//color2
+
+// band color 3
 multiplier.addEventListener('click', (e) => {
   color = e.target.className.replace('box ', '');
   resistorBands3.className = `band ${color}`;
   defaultBands.multiplier = color;
   answerElement.innerText = getResistorOhms(defaultBands);
 });
-//color 3
+
+// band color 4
 tolerance.addEventListener('click', (e) => {
   color = e.target.className.replace('box ', '');
   resistorBands4.className = `band ${color}`;
