b0bf646994930ac6e5ade7ad3ea03d47f354403f
Maglen-gg <bryan.with.a.wi@gmail.com>
2022-04-10 12:45:47 -0400

    Finished Resistor

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # Resistor
+
+Click on the different bands for Band1, Band2, Multiplier & Tolerance to get different resistor values
+
+## Band Values
+
+Below are each band and their values based on the band color
+
+### Band One
+
+| Color  | Value |
+| ------ | ----- |
+| Black  | 0     |
+| Brown  | 1     |
+| Red    | 2     |
+| Orange | 3     |
+| Yellow | 4     |
+| Green  | 5     |
+| Blue   | 6     |
+| Violet | 7     |
+| Grey   | 8     |
+| White  | 9     |
+
+### Band Two
+
+| Color  | Value |
+| ------ | ----- |
+| Black  | 0     |
+| Brown  | 1     |
+| Red    | 2     |
+| Orange | 3     |
+| Yellow | 4     |
+| Green  | 5     |
+| Blue   | 6     |
+| Violet | 7     |
+| Grey   | 8     |
+| White  | 9     |
+
+### Band Three - Multiplier
+
+| Color  | Value |
+| ------ | ----- |
+| Black  | x1    |
+| Brown  | x10   |
+| Red    | x100  |
+| Orange | x1k   |
+| Yellow | x10k  |
+| Green  | x100k |
+| Blue   | x1M   |
+| Violet | x10M  |
+| Grey   | x100M |
+| White  | x1G   |
+| Gold   | x0.1  |
+| Silver | x0.01 |
+
+### Band Four - Tolerance
+
+| Color  | Value   |
+| ------ | ------- |
+| Brown  | ± 1%    |
+| Red    | ± 2%    |
+| Green  | ± 0.5%  |
+| Blue   | ± 0.25% |
+| Violet | ± 0.05% |
+| Gold   | ± 5%    |
+| Silver | ± 10%   |
--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 import { getResistorOhms } from './resistor.js';
+
+const defaultBands = {
+  color1: 'red',
+  color2: 'green',
+  multiplier: 'violet',
+  tolerance: 'gold',
+};
+
+const resistorBands1 = document.querySelector('#b0');
+const resistorBands2 = document.querySelector('#b1');
+const resistorBands3 = document.querySelector('#b2');
+const resistorBands4 = document.querySelector('#b3');
+
+const band1 = document.querySelector('#color0');
+const band2 = document.querySelector('#color1');
+const multiplier = document.querySelector('#color2');
+const tolerance = document.querySelector('#color3');
+
+const answerElement = document.querySelector('.answer');
+
+let color;
+
+band1.addEventListener('click', (e) => {
+  color = e.target.className.replace('box ', '');
+  resistorBands1.className = `band ${color}`;
+  defaultBands.color1 = color;
+  answerElement.innerText = getResistorOhms(defaultBands);
+});
+
+//color 1
+
+band2.addEventListener('click', (e) => {
+  color = e.target.className.replace('box ', '');
+  resistorBands2.className = `band ${color}`;
+  defaultBands.color2 = color;
+  answerElement.innerText = getResistorOhms(defaultBands);
+});
+//color2
+multiplier.addEventListener('click', (e) => {
+  color = e.target.className.replace('box ', '');
+  resistorBands3.className = `band ${color}`;
+  defaultBands.multiplier = color;
+  answerElement.innerText = getResistorOhms(defaultBands);
+});
+//color 3
+tolerance.addEventListener('click', (e) => {
+  color = e.target.className.replace('box ', '');
+  resistorBands4.className = `band ${color}`;
+  defaultBands.tolerance = color;
+  answerElement.innerText = getResistorOhms(defaultBands);
+});
